1d4ea3355c4a6195c6292508206f0a6df6a6b4ec
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-12-29 18:17:27 -0500

    4/7: Added total contributions, money, games stats

--- index.js
+++ index.js
@@ -63,20 +63,21 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+let totalIndividualContributors = GAMES_JSON.reduce((curr, next) => curr + next.backers, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${totalIndividualContributors.toLocaleString("en-US")}`
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+let totalMoneyPledged = GAMES_JSON.reduce((curr, next) => curr + next.pledged, 0);
 
 // set inner HTML using template literal
-
+raisedCard.innerText = `$${totalMoneyPledged.toLocaleString("en-US")}`
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
-
+gamesCard.innerHTML = GAMES_JSON.length;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
