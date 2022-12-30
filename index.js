0db6fc08b844fa569cb41a3562cdfcab1a3c7e2a
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-12-30 15:08:26 -0500

    Top two games display

--- index.js
+++ index.js
@@ -159,14 +159,23 @@ descriptionContainer.appendChild(fundDisplay);
  */
 
 const firstGameContainer = document.getElementById("first-game");
+document.getElementById("first-game");
 const secondGameContainer = document.getElementById("second-game");
+document.getElementById("second-game");
 
 const sortedGames = GAMES_JSON.sort((item1, item2) => {
     return item2.pledged - item1.pledged;
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topGame = document.createElement("p");
+topGame.innerHTML = firstGame.name;
+firstGameContainer.appendChild(topGame);
 
 // do the same for the runner up item
+const runnerUp = document.createElement("p");
+runnerUp.innerHTML = secondGame.name;
+secondGameContainer.appendChild(runnerUp);
